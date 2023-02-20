c7d43f22a85b3fc69a64dd1149aaaf2db1fc886f
Angelo Poerio <angelo.poerio@tier.app>
2023-02-20 09:25:29 +0100

    run black linter on the code

--- kubetlkt/docker.py
+++ kubetlkt/docker.py
@@ -3,11 +3,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def build_image(repo,name,tag="latest") -> tuple:
+
+def build_image(repo, name, tag="latest") -> tuple:
     client = docker.from_env()
     logger.info("Building the docker image")
     try:
-        image = client.images.build(path="kubetlkt",tag=repo+"/" + name + ":"+tag)[0]
+        image = client.images.build(path="kubetlkt", tag=repo + "/" + name + ":" + tag)[
+            0
+        ]
     except docker.errors.BuildError as err:
         logger.error("Image build failed")
         logger.error(err)
@@ -15,17 +18,18 @@ def build_image(repo,name,tag="latest") -> tuple:
         logger.error("Docker API error")
         logger.error(err)
     else:
-        logger.info("Docker image " + repo+":"+tag +" built successfully")
+        logger.info("Docker image " + repo + ":" + tag + " built successfully")
         return image
+
 
 def push_image(repo, name, tag="latest"):
     client = docker.from_env()
     logger.info("Pushing the docker image to the registry")
     resp = client.api.push(
-    repo+"/"+name,
-    tag="latest",
-    stream=True,
-    decode=True,
+        repo + "/" + name,
+        tag="latest",
+        stream=True,
+        decode=True,
     )
     for line in resp:
         logger.info(line)
